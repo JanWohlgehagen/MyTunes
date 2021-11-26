5c976b78bd8afd2ac274fbba4cb2c501d4de01df
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2021-11-26 12:24:38 +0100

    Created Main Class (does not run)

---
+++ src/gui/view/App.java
@@ -0,0 +1,25 @@
+package gui.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
