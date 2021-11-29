cabd9ff44f651fcb69fa2318cc170cb8a4aa090a
magn20 <magnus.overgaard@live.dk>
2021-11-29 13:38:12 +0100

    gui update
    added in all the controls in scenebuilder¨,

--- src/gui/controller/MainController.java
+++ src/gui/controller/MainController.java
@@ -1,4 +1,11 @@
 package gui.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
 }
+
