7133bb3e71f921f36c594399e95f7bff0ed778c1
Tobias Rasmussen <91528477+TobiasEASV@users.noreply.github.com>
2021-11-30 09:38:04 +0100

    upps

--- src/be/Playlist.java
+++ src/be/Playlist.java
@@ -1,4 +1,28 @@
 package be;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
+
+    private String name;
+    private List<Song> songList;
+
+    public Playlist(String name){
+        this.name = name;
+        this.songList = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Song> getSongList() {
+        return songList;
+    }
+
+    public void addSongToPlayList(Song song){
+        this.songList.add(song);
+    }
 }
